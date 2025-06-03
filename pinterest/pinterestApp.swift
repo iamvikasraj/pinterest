895458ecfc36cf3fc2e4e5204ca49082a5b5745c
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-06-03 13:21:00 +0530

    Initial Commit

---
+++ pinterest/pinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinterestApp.swift
+//  pinterest
+//
+//  Created by Vikas Raj Yadav on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct pinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pinterestTests/pinterestTests.swift
@@ -0,0 +1,16 @@
+//
+//  pinterestTests.swift
+//  pinterestTests
+//
+//  Created by Vikas Raj Yadav on 03/06/25.
+//
+
+import Testing
+
+struct pinterestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
